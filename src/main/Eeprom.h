445591cae0b278d090f82b967761dea472e02a62
Malik Babiker <malikanas553@gmail.com>
2024-12-04 21:37:25 +0200

    Added EEPROM read and write block functions

--- src/main/Eeprom.h
+++ src/main/Eeprom.h
@@ -2,8 +2,12 @@
 #define __EEPROM__ 
 
 /* function for clock */
-void EEP_Write(unsigned char address, char value);
+void EEP_Write_Byte(unsigned char address, char value);
 
-unsigned char EEP_Read(unsigned char address);
+unsigned char EEP_Read_Byte(unsigned char address);
+
+void EEP_Read_Block(void *__dst, const void *__src, size_t __n);
+
+void EEP_Write_Block(const void *__src, void *__dst, size_t __n);
 
 #endif /*#ifndef __EEPROM__*/
--- src/main/Eeprom.ino
+++ src/main/Eeprom.ino
@@ -1,17 +1,32 @@
 #include "Eeprom.h"
 #include<avr/eeprom.h>
 
+
 /* function for clock */
-void EEP_Write(unsigned char address, char value)
+void EEP_Write_Byte(unsigned char address, char value)
 {
 
-	eeprom_write_byte ((uint8_t*) address, value); 
+	 eeprom_write_byte ((uint8_t*) address, value); 
   
 }
 
-unsigned char EEP_Read(unsigned char address)
+unsigned char EEP_Read_Byte(unsigned char address)
 {
 	unsigned char byteRead;
 	byteRead = eeprom_read_byte((uint8_t*)address); // read the byte in location 23
 	return (unsigned char)byteRead;
 }
+
+void EEP_Read_Block(void *__dst, const void *__src, size_t __n) {
+
+    eeprom_read_block(__dst, __src, __n); // Call the underlying EEPROM library function
+
+}
+
+void EEP_Write_Block(const void *__src, void *__dst, size_t __n) {
+
+    eeprom_write_block(__src, __dst, __n);
+    
+}
+
+
