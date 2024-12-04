9c1f9804969d58f5637c6ab0e6109acba9705524
sseifsalama <126957932+sseifsalama@users.noreply.github.com>
2024-12-04 17:09:01 +0200

    Added PWM Channels

--- src/main/Pwm.h
+++ src/main/Pwm.h
@@ -1,16 +1,11 @@
 #ifndef __Pwm__
 #define __Pwm__
 
+// Timer 0
 #define OCR0A  (*(volatile unsigned char*)0x27)
 #define OCR0B  (*(volatile unsigned char*)0x28)
-#define OCR1AL (*(volatile unsigned char*)0x88)
-#define OCR1AH (*(volatile unsigned char*)0x89)
-#define OCR1BL (*(volatile unsigned char*)0x8A)
-#define OCR1BH (*(volatile unsigned char*)0x8B)
-#define OCR2A  (*(volatile unsigned char*)0xB3)
-#define OCR2B  (*(volatile unsigned char*)0xB4)
-
 #define TCCR0A (*(volatile unsigned char*)0x24)
+#define TCCR0B (*(volatile unsigned char*)0x25)
 
 #define WGM00  0
 #define WGM01  1
@@ -18,15 +13,57 @@
 #define COM0B1 5
 #define COMOA0 6
 #define COM0A1 7
-
-#define TCCR0B (*(volatile unsigned char*)0x25)
-
 #define CS00  0
 #define CS01  1
 #define CS02  2
 #define WGM02 3
 #define FOC0B 6
 #define FOC0A 7
+
+// Timer 1
+#define OCR1AL (*(volatile unsigned char*)0x88)
+#define OCR1AH (*(volatile unsigned char*)0x89)
+#define OCR1BL (*(volatile unsigned char*)0x8A)
+#define OCR1BH (*(volatile unsigned char*)0x8B)
+#define TCCR1A (*(volatile unsigned char*)0x80)
+#define TCCR1B (*(volatile unsigned char*)0x81)
+
+#define WGM10  0
+#define WGM11  1
+#define COM1B0 4
+#define COM1B1 5
+#define COM1A0 6
+#define COM1A1 7
+#define CS10   0
+#define CS11   1
+#define CS12   2
+#define WGM12  3
+#define WGM13  4
+
+// Timer 2
+#define OCR2A  (*(volatile unsigned char*)0xB3)
+#define OCR2B  (*(volatile unsigned char*)0xB4)
+#define TCCR2A (*(volatile unsigned char*)0xB0)
+#define TCCR2B (*(volatile unsigned char*)0xB1)
+
+#define WGM20  0
+#define WGM21  1
+#define COM2B0 4
+#define COM2B1 5
+#define COM2A0 6
+#define COM2A1 7
+#define CS20   0
+#define CS21   1
+#define CS22   2
+
+typedef enum {
+    PWM_CHANNEL_0A,  // Timer 0, OC0A
+    PWM_CHANNEL_0B,  // Timer 0, OC0B
+    PWM_CHANNEL_2A,  // Timer 2, OC2A
+    PWM_CHANNEL_2B,  // Timer 2, OC2B
+    PWM_CHANNEL_1A,  // Timer 1, OC1A
+    PWM_CHANNEL_1B   // Timer 1, OC1B
+} PWM_Channel;
 
 void PWM_Init(void);
 void PWM_SetDutyCycle(unsigned char duty);
--- src/main/Pwm.ino
+++ src/main/Pwm.ino
@@ -2,26 +2,100 @@
 #include "dio.h"
 #include "register.h"
 
-void PWM_Init(){
-  Set_PIN_Direction(&DDRD, PD6, DIO_OUTPUT); //Set OC0A as output
-  
-  // Configure TCCR0A for Non-Inverting Fast PWM mode 3 using OCR A unit
-  Register_SetBit(&TCCR0A, COM0A1);
-  Register_SetBit(&TCCR0A, WGM01);
-  Register_SetBit(&TCCR0A, WGM00);
+void PWM_Init(PWM_Channel channel) {
+    switch (channel) {
+        case PWM_CHANNEL_0A:
+            Set_PIN_Direction(&DDRD, PD6, DIO_OUTPUT); // OC0A on PD6 (Pin 6)
+            Register_SetBit(&TCCR0A, COM0A1); // Non-inverting Fast PWM
+            Register_SetBit(&TCCR0A, WGM01);  // Fast PWM mode
+            Register_SetBit(&TCCR0A, WGM00);  
+            Register_SetBit(&TCCR0B, CS00);   // Prescaler 1024
+            Register_SetBit(&TCCR0B, CS02);
+            break;
 
-  // Configure TCCR0B for a prescaler of 1024
-  Register_SetBit(&TCCR0B, CS00);
-  Register_SetBit(&TCCR0B, CS02);
+        case PWM_CHANNEL_0B:
+            Set_PIN_Direction(&DDRD, PD5, DIO_OUTPUT); // OC0B on PD5 (Pin 5)
+            Register_SetBit(&TCCR0A, COM0B1); // Non-inverting Fast PWM
+            Register_SetBit(&TCCR0A, WGM01);  // Fast PWM mode
+            Register_SetBit(&TCCR0A, WGM00);
+            Register_SetBit(&TCCR0B, CS00);   // Prescaler 1024
+            Register_SetBit(&TCCR0B, CS02);
+            break;
+
+        case PWM_CHANNEL_1A:
+            Set_PIN_Direction(&DDRB, PB1, DIO_OUTPUT); // OC1A on PB1 (Pin 9)
+            Register_SetBit(&TCCR1A, COM1A1); // Non-inverting Fast PWM
+            Register_SetBit(&TCCR1A, WGM11);  // Fast PWM mode
+            Register_SetBit(&TCCR1A, WGM10);
+            Register_SetBit(&TCCR1B, WGM13);  
+            Register_SetBit(&TCCR1B, WGM12);
+            Register_SetBit(&TCCR1B, CS10);   // Prescaler 1
+            break;
+
+        case PWM_CHANNEL_1B:
+            Set_PIN_Direction(&DDRB, PB2, DIO_OUTPUT); // OC1B on PB2 (Pin 10)
+            Register_SetBit(&TCCR1A, COM1B1); // Non-inverting Fast PWM
+            Register_SetBit(&TCCR1A, WGM11);  // Fast PWM mode
+            Register_SetBit(&TCCR1A, WGM10);
+            Register_SetBit(&TCCR1B, WGM13);  
+            Register_SetBit(&TCCR1B, WGM12);
+            Register_SetBit(&TCCR1B, CS10);   // Prescaler 1
+            break;
+
+        case PWM_CHANNEL_2A:
+            Set_PIN_Direction(&DDRD, PD3, DIO_OUTPUT); // OC2A on PD3 (Pin 3)
+            Register_SetBit(&TCCR2A, COM2A1); // Non-inverting Fast PWM
+            Register_SetBit(&TCCR2A, WGM21);  // Fast PWM mode
+            Register_SetBit(&TCCR2A, WGM20);
+            Register_SetBit(&TCCR2B, CS20);   // Prescaler 1024
+            Register_SetBit(&TCCR2B, CS22);
+            break;
+
+        case PWM_CHANNEL_2B:
+            Set_PIN_Direction(&DDRB, PB3, DIO_OUTPUT); // OC2B on PB3 (Pin 11)
+            Register_SetBit(&TCCR2A, COM2B1); // Non-inverting Fast PWM
+            Register_SetBit(&TCCR2A, WGM21);  // Fast PWM mode
+            Register_SetBit(&TCCR2A, WGM20);
+            Register_SetBit(&TCCR2B, CS20);   // Prescaler 1024
+            Register_SetBit(&TCCR2B, CS22);
+            break;
+
+        default:
+            break;
+    }
 }
 
-void PWM_SetDutyCycle(char duty,unsigned char pin){
-  if (pin == 3) {
-    OCR2B = duty;
-  } else if (pin == 6) {
-    OCR0A = duty;
-  } else {
-    //invalid pin
-    return;
-  }
+
+void PWM_SetDutyCycle(PWM_Channel channel, uint8_t duty) {
+    switch (channel) {
+        case PWM_CHANNEL_0A:
+            OCR0A = duty;
+            break;
+
+        case PWM_CHANNEL_0B:
+            OCR0B = duty;
+            break;
+
+        case PWM_CHANNEL_1A:
+            OCR1AL = duty & 0xFF;
+            OCR1AH = (duty >> 8); 
+            break;
+
+        case PWM_CHANNEL_1B:
+            OCR1BL = duty & 0xFF;
+            OCR1BH = (duty >> 8);
+            break;
+
+        case PWM_CHANNEL_2A:
+            OCR2A = duty;
+            break;
+
+        case PWM_CHANNEL_2B:
+            OCR2B = duty;
+            break;
+
+        default:
+            // Handle invalid channels if needed
+            break;
+    }
 }
