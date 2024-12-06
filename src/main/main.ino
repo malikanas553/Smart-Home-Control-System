a3bbeed6a921c32c2d85788385f47f4b9c40ec1d
Malik Babiker <malikanas553@gmail.com>
2024-12-06 18:21:51 +0200

    Changed Navigation keys and added DC Speed Control

--- src/main/main.ino
+++ src/main/main.ino
@@ -9,10 +9,13 @@
 #include <stdlib.h>
 
 
-#define SAMPLE_N0 200
+#define SAMPLE_N0 50
 #define CW 0
 #define CCW 1
 #define BACK 2
+#define TEMP_ADDRESS 0
+#define SPEED_ADDRESS 2
+#define DIR_ADDRESS 4
 
 unsigned char buffer[5];  // Increased buffer size for safety
 
@@ -21,13 +24,10 @@ uint8_t channel; // Variable for choosing a channel
 unsigned char samples[SAMPLE_N0]; // Array to store LDR values
 uint8_t sample_index = 0; // Index of Samples Array
 unsigned int FAN_SPEED = 100;
-unsigned char FAN_DIR = 0;
+unsigned char FAN_DIR = CW;
 unsigned char AC_TEMP = 20;
 unsigned char new_setting = 0;
 
-#define TEMP_ADDRESS 0
-#define SPEED_ADDRESS 2
-#define DIR_ADDRESS 4
 
 void RestoreSettings() {
     // Read FAN_SPEED (2 bytes)
@@ -54,8 +54,8 @@ void SaveSettings() {
 void AC_Menu(){
     
     LCD_String("TMP:");
-    LCD_String_xy(1, 0, "7:SPEED");
-    LCD_String_xy(1,13,"9:");
+    LCD_String_xy(1, 0, "1:S/D 2:SET");
+    LCD_String_xy(1,13,"3:");
     LCD_Send(BACK,MODE_DATA);
 
 }
@@ -66,6 +66,7 @@ void init() {
   LCD_Init();
   UART_Init(9600);
   Adc_Init();
+  DC_Init();
   LCD_Custom_Char(CW,CW_Arrow);
   LCD_Custom_Char(CCW,CCW_Arrow);
   LCD_Custom_Char(BACK,Back_Arrow);
@@ -78,6 +79,7 @@ int main(void) {
   init();
 
   while (1) {
+    //System Initialization
     LCD_Clear();
     UART_SendString("System Started\n");
     LCD_Send(DISPLAY_ON_CURSOR_OFF, MODE_COMMAND);
@@ -89,13 +91,15 @@ int main(void) {
     while (key != 1 && key != 2) { // Wait for a key to get pressed
       key = keypad_get_key();
     }
-
+ 
     switch (key) {
       case 1:
           channel = TMP_PIN;
           UART_SendString("AC Chosen\n");
           LCD_Clear();
           AC_Menu();
+          _delay_ms(150);
+          key = 0;
           break;
 
       case 2:
@@ -114,10 +118,14 @@ int main(void) {
     
 
     
-    while (key != 9) {
-
+    while (key != 3) {
+      
+      if(channel == TMP_PIN){ //Add Fan direction if AC chosen
         LCD_MoveCursor_xy(0,14);
         LCD_Send(FAN_DIR,MODE_DATA);
+      }
+
+       //Sampling for adc_reading
         uint16_t adc_reading = Adc_ReadChannel(channel);
         samples[sample_index] = adc_reading;
         sample_index++;
@@ -128,46 +136,54 @@ int main(void) {
         }
         avg /= SAMPLE_N0;
         adc_reading = avg;
-
+       
      
 
-      // Display sensor reading and Send through UART
-
+      // Display sensor reading
       itoa(adc_reading, buffer, 10);
       LCD_String_xy(0, 4, buffer);
-
-      if (key == 6){
-
-       FAN_DIR = (FAN_DIR) ? CW : CCW;
-       UART_SendString("Fan Direction Toggled\n");
-       _delay_ms(150);
-       new_setting = 1;
-
-
-     } else if (key == 7){
+ 
+       if (key == 1){
+          DC_Start(0, FAN_DIR, FAN_SPEED);
+          key = '\0';
           LCD_Clear();
           LCD_String("SPEED:");
           UART_SendString("AC Speed Option Chosen\n");
           LCD_MoveCursor_xy(0,14);
           LCD_Send(FAN_DIR,MODE_DATA);
-          LCD_String_xy(1,13,"9:");
+          LCD_String_xy(1,0,"4:- 5:+ 6:");
+          LCD_Send(CW,MODE_DATA);
+          LCD_String_xy(1,13,"3:");
           LCD_Send(BACK,MODE_DATA);
 
-          while (key != 9){ // Wait for a key to get pressed
+          while (key != 3){ // Wait for a key to get pressed
+          
             unsigned char speed[5];
             itoa(FAN_SPEED,speed,10);
-            if(key == 5){
+            LCD_MoveCursor_xy(0,14);
+            LCD_Send(FAN_DIR,MODE_DATA);
+
+            if (key == 6){
+                FAN_DIR = (FAN_DIR) ? CW : CCW;
+                UART_SendString("Fan Direction Toggled\n");
+                _delay_ms(150);
+                new_setting = 1;
+            }else if(key == 5){
+
               if(FAN_SPEED+100 < 1100){
               FAN_SPEED += 100;
+              DC_SetSpeed(0 , FAN_SPEED);
               UART_SendString("Fan Speed Increased\n");
               new_setting = 1;
               }else{
               UART_SendString("Fan Speed is at Maximum\n");
               }
               _delay_ms(150);
+
             }else if(key == 4){
                if(FAN_SPEED-100 > 0){
                  FAN_SPEED -= 100;
+                 DC_SetSpeed(0 , FAN_SPEED);
                  UART_SendString("Fan Speed Decreased\n");
                  new_setting = 1;
               }else{
@@ -193,7 +209,55 @@ int main(void) {
           LCD_Clear();
           AC_Menu();
 
-     }
+     } else if(key == 2){
+
+          LCD_Clear();
+          LCD_String("TEMP:");
+          UART_SendString("AC Temp Option Chosen\n");
+          LCD_String_xy(1,0,"4:- 5:+");
+          LCD_String_xy(1,13,"3:");
+          LCD_Send(BACK,MODE_DATA);
+           while (key != 3){ // Wait for a key to get pressed
+          
+            unsigned char temp[5];
+            itoa(AC_TEMP,temp,10);
+            if(key == 5){
+              if(AC_TEMP + 1 < 27){
+              AC_TEMP += 1;
+              UART_SendString("AC Temp Increased\n");
+              new_setting = 1;
+              }else{
+              UART_SendString("AC Temp is at Maximum\n");
+              }
+              _delay_ms(150);
+            }else if(key == 4){
+
+               if(AC_TEMP - 1 > 19){
+                 AC_TEMP -= 1;
+                 UART_SendString("AC Temp Decreased\n");
+                 new_setting = 1;
+              }else{
+                 UART_SendString("AC Temp is at Minimum\n");
+              }
+              _delay_ms(150);
+
+            }
+            LCD_String_xy(0,5,temp);
+            key = keypad_get_key();
+            _delay_ms(3);
+            LCD_String_xy(0,5,"    ");
+        }
+           if(new_setting){
+            SaveSettings();
+            UART_SendString("Settings Saved");
+            new_setting = 0;
+          }
+          key = '\0';
+          _delay_ms(150);
+          LCD_Clear();
+          AC_Menu();
+          
+     } 
 
      
 
