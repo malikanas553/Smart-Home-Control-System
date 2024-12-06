ffb50ef20172725992cb156aac0358b3ca70d3d0
Malik Babiker <malikanas553@gmail.com>
2024-12-06 18:20:50 +0200

    Added Set Speed and Direction Functions

--- src/main/DC_Motor.h
+++ src/main/DC_Motor.h
@@ -7,7 +7,8 @@ typedef enum {
 } tenuDirection;
 
 void DC_Init();
-void DC_Start(unsigned char MotorID, tenuDirection Direction, unsigned char Speed);
+void DC_Start(unsigned char MotorID, unsigned char Direction, unsigned char Speed);
+void DC_SetSpeed(unsigned char MotorID,unsigned char Speed);
 void DC_Stop(unsigned char MotorID);
 
 #endif
--- src/main/DC_Motor.ino
+++ src/main/DC_Motor.ino
@@ -2,38 +2,56 @@
 #include "Pwm.h"
 #include "dio.h"
 // Motor Array[Motor No][Pin]
-unsigned char MotorArray[][3] = {
-  {4, 5, 6}
+unsigned char MotorArray[][4] = {
+  {PC0, PC1, PB2, PWM_CHANNEL_1B}
 };
 
 void DC_Init(){
 
-    Set_PIN_Direction(&DDRD, MotorArray[0][0], DIO_OUTPUT);
-    Set_PIN_Direction(&DDRD, MotorArray[0][1], DIO_OUTPUT);
-    //PWM_Init();
+    Set_PIN_Direction(&DDRC, MotorArray[0][0], DIO_OUTPUT);
+    Set_PIN_Direction(&DDRC, MotorArray[0][1], DIO_OUTPUT);
+    Set_PIN_Direction(&DDRB, MotorArray[0][2], DIO_OUTPUT);
+    PWM_Init(MotorArray[0][4]);
 
 }
 
 
-void DC_Start(unsigned char MotorID, tenuDirection Direction, unsigned char Speed){
+void DC_Start(unsigned char MotorID, unsigned char Direction, unsigned char Speed){
 
-    if (!DIRECTION_CW)
+    if (!Direction)
     {
-      Set_PIN_State(&PORTD, MotorArray[MotorID][0], HIGH);
-      Set_PIN_State(&PORTD, MotorArray[MotorID][1], LOW);
-      PWM_SetDutyCycle(Speed,MotorArray[MotorID][2]);
+      Set_PIN_State(&PORTC, MotorArray[MotorID][0], HIGH);
+      Set_PIN_State(&PORTC, MotorArray[MotorID][1], LOW);
+      PWM_SetDutyCycle(Speed,MotorArray[MotorID][4]);
 
     } else{
-      Set_PIN_State(&PORTD, MotorArray[MotorID][0], LOW);
-      Set_PIN_State(&PORTD, MotorArray[MotorID][1], HIGH);
-      PWM_SetDutyCycle(Speed,MotorArray[MotorID][2]);
+      Set_PIN_State(&PORTC, MotorArray[MotorID][0], LOW);
+      Set_PIN_State(&PORTC, MotorArray[MotorID][1], HIGH);
+      PWM_SetDutyCycle(Speed,MotorArray[MotorID][4]);
     }
 
 }
+void DC_SetSpeed(unsigned char MotorID,unsigned char Speed){
 
+  PWM_SetDutyCycle(Speed,MotorArray[MotorID][4]);
+
+}
+
+void DC_SetDirection(unsigned char MotorID, unsigned char Direction){
+    if (!Direction)
+    {
+      Set_PIN_State(&PORTC, MotorArray[MotorID][0], HIGH);
+      Set_PIN_State(&PORTC, MotorArray[MotorID][1], LOW);
+
+    } else{
+      Set_PIN_State(&PORTC, MotorArray[MotorID][0], LOW);
+      Set_PIN_State(&PORTC, MotorArray[MotorID][1], HIGH);
+    }
+
+}
 void DC_Stop(unsigned char MotorID){
 
-    Set_PIN_State(&PORTD, MotorArray[MotorID][0], HIGH);
-    Set_PIN_State(&PORTD, MotorArray[MotorID][1], HIGH);
+    Set_PIN_State(&PORTC, MotorArray[MotorID][0], HIGH);
+    Set_PIN_State(&PORTC, MotorArray[MotorID][1], HIGH);
     
 }
