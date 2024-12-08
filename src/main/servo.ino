35163ebbd3fa008f7a292ad656b89e7626284548
Maro-23 <marwanhatem1234@gmail.com>
2024-12-08 22:28:20 +0200

    integrated PWM library in Servo library

--- src/main/servo.ino
+++ src/main/servo.ino
@@ -1,38 +1,37 @@
 #include "servo.h"
 #include "Register.h"
 #include "dio.h"
+#include "Pwm.h"
 
 // Initialize the servo control
-void servo_init(void) {
+void servo_init(void)
+{
     // Set PB1 (OC1A) as output
-    Register_SetBit(&DDRB, 1);
-
-    // Configure Timer1 for Fast PWM mode
-    Register_SetBit(&TCCR1A, WGM11);
-    Register_SetBit(&TCCR1A, COM1A1);
-    Register_SetBit(&TCCR1B, WGM13);
-    Register_SetBit(&TCCR1B, WGM12);
-    Register_SetBit(&TCCR1B, CS11);
-
+    PWM_Init(PWM_CHANNEL_0B_PD5);
     // Set the TOP value for a 20ms period
     ICR1 = TIMER_TOP;
 }
 
 // Set the angle of the servo
-void servo_set_angle(uint8_t angle) {
-    if (angle > 180) angle = 180;  // Limit angle to 180 degrees
+void servo_set_angle(uint8_t angle)
+{
+    if (angle > 180)
+        angle = 180; // Limit angle to 180 degrees
     uint16_t pulse_width = SERVO_MIN_PULSE + ((uint32_t)(SERVO_MAX_PULSE - SERVO_MIN_PULSE) * angle) / 180;
-    
+
     // Convert the pulse width to timer ticks
-    OCR1A = (16 * pulse_width) / PRESCALER;
+    PWM_SetDutyCycle(PWM_CHANNEL_0B_PD5, ((16 * pulse_width) / PRESCALER));
 }
 
-void servo_swing(void) {
-    for (uint8_t i = 0; i <= 180; i++) {
+void servo_swing(void)
+{
+    for (uint8_t i = 0; i <= 180; i++)
+    {
         servo_set_angle(i);
         _delay_ms(10);
     }
-    for (uint8_t i = 180; i > 0; i--) {
+    for (uint8_t i = 180; i > 0; i--)
+    {
         servo_set_angle(i);
         _delay_ms(10);
     }
