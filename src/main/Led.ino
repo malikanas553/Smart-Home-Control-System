5006426251836e19f5e3c932b76d98a199fa65c9
Malik Babiker <malikanas553@gmail.com>
2024-12-08 21:27:35 +0200

    Fixed LED Functions

--- src/main/Led.ino
+++ src/main/Led.ino
@@ -5,20 +5,19 @@
 
 void LED_Init(void){
 
-    Set_PIN_Direction(&DDRD, YELLOW_LED_PIN, DIO_OUTPUT);
     PWM_Init(PWM_CHANNEL_2B_PD3);
 
 }
 
 
 void LED_ON(unsigned char Brightness){
-
+    LED_Init();
     PWM_SetDutyCycle(PWM_CHANNEL_2B_PD3,Brightness);
   
 }
 
 void LED_OFF(void){
-     
-     Set_PIN_State(&PORTD, YELLOW_LED_PIN , LOW);
+  
+    PWM_Disable(PWM_CHANNEL_2B_PD3);
      
 }
